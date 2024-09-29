00d0fd8527352b419d9c96b364e70d90fcea2658
Dmitri-Tro <dtroshchenkov@gmail.com>
2024-09-29 15:40:49 +0300

    implemented Error page
    - implemented Error page
    - created hook useGoBack

---
+++ src/hooks/useGoBack.ts
@@ -0,0 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
+export const useGoBack = () => {
+
+  const navigate = useNavigate();
+
+  return () => {
+    if ( window.history?.length && window.history.length > 1 ) {
+      navigate( -1 );
+    } else {
+      navigate( '/', { replace: true } );
+    }
+  };
+};
